3e7dffcc221db3a15a2ee1c4ca806e4f1ff14ea2
Tejas Bhong <tejasdbhong@gmail.com>
2024-02-08 18:41:46 +0530

    Made ready for publishing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,16 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.android.library") version "8.2.0" apply false
+    `maven-publish`
+}
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.tejas-bhong"
+            artifactId = "VerticalMaterialSlider"
+            version = "v1.0.0"
+
+            artifact("$buildDir/outputs/aar/${artifactId}-release.aar")
+        }
+    }
 }

17caff606bc28ff9a13438a30fda7c3e184db2f9
Tejas Bhong <tejasdbhong@gmail.com>
2024-02-09 22:51:25 +0530

    Prepared for publishing.

--- VerticalMaterialSlider/build.gradle.kts
+++ VerticalMaterialSlider/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -40,4 +41,26 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+android {
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["release"])
+                groupId = "com.github.tejas-bhong"
+                artifactId = "VerticalMaterialSlider"
+                version = "v1.0.0"
+            }
+        }
+    }
 }
